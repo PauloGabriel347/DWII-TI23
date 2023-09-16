aea47531f4f481a6bf48ac46e14fd4f98f3053cb
PauloGabriel347 <83320713+PauloGabriel347@users.noreply.github.com>
2023-09-15 21:36:40 -0300

    1 file modified, 1 added

---
+++ Aula2/calculadora.js
@@ -0,0 +1,10 @@
+let d = new Date();
+let anoAtual = d.getFullYear();
+let anoNascimento = 1999;
+let resultado = 0;
+let idade = 0;
+
+resultado = anoAtual - anoNascimento;
+idade += resultado;
+
+console.log(`Sua idade é ${idade}`);
---
+++ Aula2/musicas.js
@@ -0,0 +1,5 @@
+const musicas = ['Patient Number 9', 'ChainsawBlood', 'Take a chance on me'];
+musicas.push('FEIN', 'Hummingbird');
+for (musica of musicas) {
+  console.log(musica);
+}
