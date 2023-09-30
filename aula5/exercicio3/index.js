fa0a01160e5c92f7f937f3d8a3c9c5b8401a86d9
PauloGabriel347 <83320713+PauloGabriel347@users.noreply.github.com>
2023-09-29 22:27:22 -0300

    2 files modified

--- aula5/exercicio3/index.js
+++ aula5/exercicio3/index.js
@@ -1,42 +1,42 @@
 const filmesArray = [
   {
-    nome: "wild wild west",
+    nome: 'wild wild west',
     anoLancamento: 1999,
-    atores: [
-    "Will Smith",
-    "Salma Hayek"
-    ],
-    poster: 'https://m.media-amazon.com/images/M/MV5BYmYyMTc4YjItMGNhNC00OWQwLWJhMWUtNTdjZDgxMDI5MjE2L2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg'
+    atores: ['Will Smith', 'Salma Hayek'],
+    poster:
+      'https://m.media-amazon.com/images/M/MV5BYmYyMTc4YjItMGNhNC00OWQwLWJhMWUtNTdjZDgxMDI5MjE2L2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
   },
   {
-    nome: "Besouro Azul",
+    nome: 'Besouro Azul',
     anoLancamento: 2023,
-    atores: [
-      "Bruna Marquezine",
-      "George Lopez"
-    ],
-    poster: ""
+    atores: ['Bruna Marquezine', 'George Lopez'],
+    poster: '',
   },
   {
-    nome: "Speed Racer",
+    nome: 'Speed Racer',
     anoLancamento: 2008,
-    atores: [
-      "Christina Ricci",
-      "John Goodman"
-    ],
-    poster: ""
-  }
-]
+    atores: ['Christina Ricci', 'John Goodman'],
+    poster: '',
+  },
+];
 
-const id = document.querySelector("#filmes");
+const id = document.querySelector('#filmes');
 
-
-for (filme of filmesArray){
+for (filme of filmesArray) {
   const img = document.createElement('img');
   img.src = filme.poster;
   filmes.appendChild(img);
- 
-  const p1 = document.createElement('p1');
-  p1.textContent = filme.nome;
-  filmes.appendChild(p1);
+
+  const br = document.createElement('br');
+  filmes.appendChild(br);
+
+  const p = document.createElement('p');
+  p.textContent = 'Nome:';
+  filmes.appendChild(p);
+
+  const spam = document.createElement('spam');
+  spam.textContent = filme.nome;
+  filmes.appendChild(spam);
+
+  filmes.appendChild(br);
 }
