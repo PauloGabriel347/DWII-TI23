c7b1c635c03b72f09129b8f1f943ec1f2f61dbd9
Paulo Gabriel <83320713+PauloGabriel347@users.noreply.github.com>
2023-10-05 21:50:15 -0300

    Update index.js
    odeio break não funciona socorro

--- aula5/exercicio3/index.js
+++ aula5/exercicio3/index.js
@@ -7,18 +7,21 @@ const filmesArray = [
       'https://m.media-amazon.com/images/M/MV5BYmYyMTc4YjItMGNhNC00OWQwLWJhMWUtNTdjZDgxMDI5MjE2L2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
   },
   {
-    nome: 'Besouro Azul',
+    nome: 'Homem-Aranha: Através do Aranhaverso',
     anoLancamento: 2023,
-    atores: ['Bruna Marquezine', 'George Lopez'],
-    poster: '',
+    atores: ['Oscar Isaac', 'Shameik Moore'],
+    poster: 'https://assets-prd.ignimgs.com/2023/05/26/spider-man-across-the-spider-verse-chinese-poster-1685120998779.jpg',
   },
   {
     nome: 'Speed Racer',
     anoLancamento: 2008,
     atores: ['Christina Ricci', 'John Goodman'],
-    poster: '',
+    poster: 'https://i.pinimg.com/736x/82/00/7e/82007e0b8f7e53e4b7db1ac358286137.jpg',
   },
 ];
+
+const d = new Date();
+let ano = d.getFullYear();
 
 const id = document.querySelector('#filmes');
 
@@ -26,7 +29,7 @@ for (filme of filmesArray) {
   const img = document.createElement('img');
   img.src = filme.poster;
   filmes.appendChild(img);
-
+  
   const br = document.createElement('br');
   filmes.appendChild(br);
 
@@ -37,6 +40,18 @@ for (filme of filmesArray) {
   const spam = document.createElement('spam');
   spam.textContent = filme.nome;
   filmes.appendChild(spam);
-
+  
   filmes.appendChild(br);
+  
+  const p2 = document.createElement('p2');
+  p2.textContent = 'Idade:';
+  filmes.appendChild(p2);
+  
+  const idade = document.createElement('idade');
+  idade.textContent = ano - filme.anoLancamento;
+  filmes.appendChild(idade);
+  
+  filmes.appendChild(br);
+
+  
 }
